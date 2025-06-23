55d3e8d26779e11954c982eb532647d47590cb6b
humphreyotieno1 <humphreyotieno04@gmail.com>
2025-06-23 12:42:11 +0300

    Admin page and Databases

--- blog/blogwebsite/views.py
+++ blog/blogwebsite/views.py
@@ -1,31 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2023'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2023'
-    },
-    {        
-        'author': 'John Smith',
-        'title': 'Blog Post 3',
-        'content': 'Third post content',
-        'date_posted': 'August 29, 2023'
-    }
-]
 
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blogwebsite/home.html', context)
 
